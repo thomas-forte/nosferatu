8e553e27a38132da77d1cc7a927f2e54771cf389
Thomas Forte <5710966+thomas-forte@users.noreply.github.com>
2024-05-29 18:52:31 -0400

    config file

--- config.py
+++ config.py
@@ -1,20 +1,17 @@
 CONFIG = {
+    # Display settings
     "site": {
         "title": "Garage Pi",
         "subtitle": "Below are two buttons that will cycle their named relays.",
         "icon": "fas fa-warehouse",
     },
+    # Gpio settings
     "gpios": [
         {"address": 2, "name": "Relay 1", "wait": 1},
         {"address": 3, "name": "Relay 2", "wait": 1},
     ],
+    # Wemo settings
     "wemos": [
-        {"address": "10.80.80.16", "name": "Living Room A", "mac": "24:f5:a2:21:45:9f"},
-        {"address": "10.80.80.18", "name": "Living Room B", "mac": "24:f5:a2:4c:c7:bb"},
-        {"address": "10.80.80.41", "name": "Bedroom Light", "mac": "24:f5:a2:49:b3:b9"},
-        {"address": "10.80.80.19", "name": "Salt Lamp", "mac": "24:f5:a2:48:17:df"},
-        {"address": "10.80.80.23", "name": "Plant Light", "mac": "24:f5:a2:48:22:23"},
-        {"address": "10.80.80.24", "name": "Office Aroura", "mac": "24:f5:a2:48:16:ed"},
-        {"address": "10.80.80.20", "name": "Kitchen Fan", "mac": "24:f5:a2:49:b3:59"},
+        {"address": "192.168.1.1", "name": "Some Wemo", "mac": "00:00:00:00:00"},
     ],
 }
